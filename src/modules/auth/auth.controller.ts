de74e482c88746175c3360c3224f97144b8e3e91
tharindu <tharindu@epitoni.com>
2026-02-15 07:28:45 +0530

    login with JWT and secure cookie auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import express from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -13,7 +14,18 @@ export class AuthController {
   }
 
   @Post('login')
-  login(@Body() loginDto: LoginDto) {
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) res: express.Response,
+  ) {
+    const { accessToken } = await this.authService.login(loginDto);
+
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 15, // 15 minutes
+    });
     return this.authService.login(loginDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,14 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import * as argon2 from 'argon2';
+import { BadRequestException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  register(dto: RegisterDto) {
-    return { message: 'Register endpoint working', data: dto };
+  constructor(private jwtService: JwtService) {}
+  private users: { email: string; passwordHash: string }[] = [];
+  async register(dto: RegisterDto) {
+    const existingUser = this.users.find((user) => user.email === dto.email);
+
+    if (existingUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const passwordHash = await argon2.hash(dto.password);
+
+    const newUser = {
+      email: dto.email,
+      passwordHash,
+    };
+
+    this.users.push(newUser);
+
+    return { message: 'User registered successfully' };
   }
 
-  login(dto: LoginDto) {
-    return { message: 'Login endpoint working', data: dto };
+  async login(dto: LoginDto) {
+    const user = this.users.find((user) => user.email === dto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const passwordValid = await argon2.verify(user.passwordHash, dto.password);
+    if (!passwordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const token = await this.jwtService.signAsync({
+      email: user.email,
+    });
+    return { accessToken: token };
   }
 }
