4552bd81f2590718f75e59a8e6a39ae5ad167a15
tharindu <tharindu@epitoni.com>
2026-02-16 10:20:14 +0530

    cors allow frontend url

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://task-management-system-frontend-pavh-2wcmrinv1.vercel.app',
+    'https://task-management-system-frontend-pavh-bpz8grfna.vercel.app',
   ];
 
   app.enableCors({
